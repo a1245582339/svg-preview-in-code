809235753d9d681a50820e56df7ace0e2f75d9c6
Zhipeng <1245582339@qq.com>
2022-05-29 12:34:54 +0800

    feat: svg

--- README.md
+++ README.md
@@ -1,70 +1,11 @@
-# svg-preview-in-code README
+# Svg Preview In Code
 
-This is the README for your extension "svg-preview-in-code". After writing up a brief description, we recommend including the following sections.
+SPIC is a plugin that can preview svg code in any type file, such as jsx/tsx, html, and svg. There is a thumbnail before your svg tag to preview the svg, and you can hover on your svg code to view the svg in popover.
 
-## Features
+![](https://i.postimg.cc/7Y55XYg1/preview.gif)
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+By default, this plugin will watch your all files change when your file actived in editor, so, to ensure good performance, you can add path config so that specify files to be watch by the plugin. 
 
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+|  Key   | Type  |  Example   | Default  
+|  ----  | ----  | ----  | ----  
+| "spic.paths"  | `null` \| `string[]` | `["src/**/icon.tsx"]` | `null`
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,98 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { XMLBuilder, XMLParser } from 'fast-xml-parser';
+import * as glob from 'glob'
+import path = require('path');
 import * as vscode from 'vscode';
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "svg-preview-in-code" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('svg-preview-in-code.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Svg Preview In Code!');
+	let timeout: NodeJS.Timer | undefined = undefined;
+	const configPaths = vscode.workspace.getConfiguration('spic').get<string[]>('paths')
+	let activeEditor = vscode.window.activeTextEditor;
+	let globPaths: string[] = []
+	const cwd = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0].uri.fsPath
+	if (configPaths && configPaths instanceof Array && cwd) {
+		globPaths = Array.from(new Set(configPaths.map(path => glob.sync(path, {
+			cwd
+		})).flat()))
+	}
+	const svgPreviewDecorationType = vscode.window.createTextEditorDecorationType({
+		after: {}
 	});
+	
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+		if (configPaths && !globPaths.some(globPath => path.join(cwd!, globPath) === activeEditor!.document.uri.fsPath)) {
+			return
+		}
+		
+		const regEx = /<svg .*?>[\s\S]*?<\/svg>/g;
+		const text = activeEditor.document.getText();
+		const svgPreviews: vscode.DecorationOptions[] = [];
+		let match;
+		while ((match = regEx.exec(text))) {
+			const startPos = activeEditor.document.positionAt(match.index);
+			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+			let svg = match[0]
 
-	context.subscriptions.push(disposable);
+			const parser = new XMLParser({
+				ignoreAttributes: false,
+        		attributeNamePrefix : "@_",
+			});
+			const builder = new XMLBuilder(
+				{
+					ignoreAttributes: false,
+					attributeNamePrefix : "@_",
+				}
+			);
+			const svgObj = parser.parse(svg);
+			const fontSize = vscode.workspace.getConfiguration('editor').get('fontSize')
+			const originalSize = {height: svgObj.svg['@_height'], width: svgObj.svg['@_width']}
+			svgObj.svg['@_width'] = fontSize
+			svgObj.svg['@_height'] = fontSize
+			const decorationSvg = builder.build(svgObj)
+			const hoverBase64 = `data:image/svg+xml;base64,${Buffer.from(svg).toString('base64')}`
+			const decorationBase64 = `data:image/svg+xml;base64,${Buffer.from(decorationSvg).toString('base64')}`
+			const hoverMessage = new vscode.MarkdownString(`![svg](${hoverBase64}|width=50)\n\n${originalSize.width}×${originalSize.height}`)
+			const decoration: vscode.DecorationOptions = {
+				range: new vscode.Range(startPos.line, startPos.character, endPos.line, endPos.character),
+				hoverMessage,
+				renderOptions: {
+					before: {
+						contentIconPath: vscode.Uri.parse(decorationBase64),
+						height: vscode.workspace.getConfiguration('editor').get('fontSize')
+					},
+				},
+			};
+			svgPreviews.push(decoration);
+		}
+		activeEditor.setDecorations(svgPreviewDecorationType, svgPreviews);
+	}
+
+	function triggerUpdateDecorations(throttle = false) {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		if (throttle) {
+			timeout = setTimeout(updateDecorations, 500);
+		} else {
+			updateDecorations();
+		}
+	}
+
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations(true);
+		}
+	}, null, context.subscriptions);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
